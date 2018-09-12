8270947c3407b75de83b8e4196e4f824e78fc691
aukustik <aukustik@yandex.ru>
2018-09-12 14:41:19 +0700

    kek
    Binary converter

---
+++ converter.py
@@ -0,0 +1,21 @@
+class ToDec:
+
+    def __init__(self, bin_val):
+        self.value = bin_val
+    
+    def convert(self):
+        for i in self.value:
+            if(i != '0' or i != '1'):
+                print('This is not a binary!')
+                return
+        self.stack = []
+        for i in self.value:
+            self.stack.insert(0,i)
+        extant = 0
+        deci = 0
+        for i in self.stack:
+            deci = deci + int(i)*(2 ** extant)
+            extant += 1
+        
+        print('Deci =', deci)
+        
---
+++ main.py
@@ -0,0 +1,6 @@
+from converter import *
+
+print('Input binary:')
+kek = input()
+kekCon = ToDec(kek)
+kekCon.convert()
